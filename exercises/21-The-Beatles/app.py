041e04fbd747b05c5edec4a621a4a2f23e661de2
samvalvi <samuelvalerin@gmail.com>
2021-03-23 01:47:09 +0000

    exercise 22

--- exercises/21-The-Beatles/app.py
+++ exercises/21-The-Beatles/app.py
@@ -1,16 +1,16 @@
 # Your code here!!
-song = ["let it be", "whisper words of wisdom", "there will be an answer"]
-
 def sing():
-    for i in range(1, 9):
+    song = ["let it be", "whisper words of wisdom", "there will be an answer"]
+    
+    for i in range(1, 10):
         if i <= 4:
             print(song[0] + ",")
         elif i == 5:
             print(song[1] + ", " + song[0] + ", " + song[0] + ",")
         elif i > 5 and i <= 8:
             print(song[0] + ",")
-        else:
-            print(song[2] + ", " + song[1])
+        elif i == 9:
+            print(song[2] + ", " + song[0])
         
 
 sing()
